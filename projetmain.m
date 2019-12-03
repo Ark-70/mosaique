8d67b67729dc0f99bfd368db71b8431237dbbc02
TRAPARIC David <david.traparic@enseirb-matmeca.fr>
2019-12-03 16:59:05 +0100

    Extraction

--- projetmain.m
+++ projetmain.m
@@ -18,9 +18,13 @@ C1(1) = rec1x(3);
 C1(2) = rec1y(3);
 D1(1) = rec1x(4);
 D1(2) = rec1y(4);
+%
+% rec1x = [B1(1) C1(1) D1(1) A1(1)];
+% rec1y = [B1(2) C1(2) D1(2) A1(2)];
+
 
 portrait = imread('img.png');
-figure, imshow(portrait);
+% figure, imshow(portrait);
 
 A2 = [1 1]; B2 = [size(portrait, 2) 1]; C2 = [size(portrait, 2) size(portrait, 1)]; D2 = [1 size(portrait, 1)];
 
@@ -39,8 +43,8 @@ Matrix = [Matrix ; construireLigneX(D1, D2)];
 Matrix = [Matrix ; construireLigneY(D1, D2)];
 
 rec2 = [A2(1) A2(2) B2(1) B2(2) C2(1) C2(2) D2(1) D2(2)]';
-rec2x = [A2(1) B2(1) C2(1) D2(1)]';
-rec2y = [A2(2) B2(2) C2(2) D2(2)]';
+rec2x = [A2(1) B2(1) C2(1) D2(1)];
+rec2y = [A2(2) B2(2) C2(2) D2(2)];
 
 H = (((Matrix'*Matrix)^-1)*Matrix')*rec2;
 %H = Matrix\rec2;
@@ -63,18 +67,18 @@ width_portrait = size(portrait, 2);
 
 bg_new = bg;
 
-for x = 1:height_bg
-    for y=1:width_bg
+for x=1:width_bg
+    for y=1:height_bg
 
-        newCoor = inv(H_matrix) * [y;x;1]; % 1 == rajout d'une dimension
+        newCoor = inv(H_matrix) * [x;y;1]; % 1 == rajout d'une dimension
         s = newCoor(3);
         new_x = floor(newCoor(1)/s);
         new_y = floor(newCoor(2)/s);
 
-        if( 0 < new_x && new_x < height_portrait && 0 < new_y && new_y < width_portrait )
+        if( 0 < new_x && new_x < width_portrait && 0 < new_y && new_y < height_portrait )
             newCoor
 
-            bg_new(x, y, :) = portrait(new_x, new_y, :);
+            bg_new(y, x, :) = portrait(new_y, new_x, :);
         end
 
     end
